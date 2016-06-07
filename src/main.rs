1ccd303883b1c5de5bfcfe6ac2333f56b01b40c8
Alon Levy <alon@pobox.com>
2016-06-07 10:56:48 +0300

    calibrated for midburn 2016 no wind. still cannot tell apart microsoft 3000.. so those will be broken or ok, 50/50, on connection. but if power remains it will be only one line switch apart. and on any reboot.

--- src/main.rs
+++ src/main.rs
@@ -79,21 +79,26 @@ fn get_sources(filter_sources: &String, filter_not_sources:&String) -> Vec<Strin
 // return S->A level, A->S level (larger first)
 fn get_levels(source: &String) -> (f64, f64)
 {
-    if source.contains("Logitech") {
-        return (-30f64, -25f64);
+    if source.contains("alsa_input.usb-Logitech_Inc._Logitech_USB_Headset_H340-00.analog-stereo") {
+        return (-33f64, -35f64);
     }
+    if source.contains("alsa_input.usb-Logitech_Logitech_USB_Headset-00.analog-mono") {
+        return (-55f64, -57f64);
+    }
+    // TODO - better identifier. serial of card?
     if source == "pulsesrc device=alsa_input.usb-C-Media_Electronics_Inc._Microsoft_LifeChat_LX-3000-00.analog-mono.2" {
-        return (-40f64, -45f64)
+        return (-32f64, -34f64)
     }
-    if source == "pulsesrc device=alsa_input.usb-C-Media_Electronics_Inc._Microsoft_LifeChat_LX-3000-00.analog-mono" {
-        return (-40f64, -45f64)
+    if source.contains("alsa_input.usb-C-Media_Electronics_Inc._Microsoft_LifeChat_LX-3000-00.analog-mono") {
+        return (-54f64, -56f64)
     }
     if source == "pulsesrc device=alsa_input.usb-Microsoft_Microsoft_LifeChat_LX-4000-00.analog-stereo" {
         return (-40f64, -45f64)
     }
-    if source == "pulsesrc device=alsa_input.usb-Generic_USB_Ear-Microphone_0000000001-00.analog-stereo" {
-        return (-56f64, -58f64)
+    if source.contains("alsa_input.usb-Generic_USB_Ear-Microphone_0000000001-00.analog-stereo") {
+        return (-50f64, -52f64)
     }
+    println!("matching default source");
     (-56f64, -58f64)
 }
 
